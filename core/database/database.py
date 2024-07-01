d67e75f8e75ecdaeae2be6bde33ae9c1fef2b422
Egor <144518433+Egor-Alpy@users.noreply.github.com>
2024-07-01 22:04:56 +0300

    Delete core directory

--- core/database/database.py
+++
@@ -1,7 +0,0 @@
-from sqlite3 import Connection, Cursor
-
-
-class DataBase:
-    def __init__(self, connect: Connection, cur: Cursor) -> None:
-        self.database = connect
-        self.cursor = cur
